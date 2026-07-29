4dabfd11f0a0bb09f550896ff2f23ef9df98e9d0
Sebastian Villa <sebasviro567@gmail.com>
2026-07-28 22:20:47 -0500

    Add_password_change_functionality_to_user_profile

--- controllers/profile_controller.py
+++ controllers/profile_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request
-from services.profile_service import actualizar_suscripcion
+from services.profile_service import actualizar_suscripcion, cambiar_password
 
 profile_bp = Blueprint(
     "profile",
@@ -15,14 +15,50 @@ def profile():
 
     if usuario is None:
         return redirect(url_for("auth.login"))
-        
+
     if request.method == "POST":
+
+        # cambiar contraseña
+
+        if "current_password" in request.form:
+
+            current_password = request.form["current_password"]
+            new_password = request.form["new_password"]
+            confirm_password = request.form["confirm_password"]
+
+            if new_password != confirm_password:
+
+                return render_template(
+                    "profile.html",
+                    current_user=session["usuario"],
+                    mensaje_password="Las contraseñas no coinciden."
+                )
+
+            if current_password == new_password:
+
+                return render_template(
+                    "profile.html",
+                    current_user=session["usuario"],
+                    mensaje_password="La nueva contraseña debe ser diferente a la actual."
+                )
+
+            mensaje = cambiar_password(
+                usuario["email"],
+                current_password,
+                new_password
+            )
+
+            return render_template(
+                "profile.html",
+                current_user=session["usuario"],
+                mensaje_password=mensaje
+            )
 
         print("Entró al POST")
 
         nuevo_plan = request.form["plan"]
 
-    # Validamos que el plan sea uno permitido
+        # Validamos que el plan sea uno permitido
         if nuevo_plan not in ["Basica", "Premium"]:
             return redirect(url_for("profile.profile"))
 
@@ -42,7 +78,7 @@ def profile():
             print("No actualizado")
 
         return redirect(url_for("profile.profile"))
-    
+
     return render_template(
         "profile.html",
         current_user=session["usuario"]
